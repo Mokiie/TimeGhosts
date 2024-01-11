148e6e70f11d638053a226a9802a994a1d30b380
DESKTOP-ITVP7RT\rowan <rowan.clover@gmail.com>
2024-01-10 17:36:37 -0800

    added movement options and some images

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,43 +1,79 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController characterController;
-
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+    private float _currentVelocity;
+    [SerializeField] private float smoothTime = 0.05f;
+    [SerializeField] private float speed;
     public Transform cam;
+    private float gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float velocity;
+    Vector3 moveDirection;
+    [SerializeField] private float jumpPower;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        
+        
+    }
     
 
-    public float speed = 5f;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        characterController = GetComponent<CharacterController>();
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ApplyGravity() 
     {
-        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-
-        if(direction.magnitude >= 0.1f)
+        if (IsGrounded() && velocity < 0.0f)
         {
-            
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
-            //Player rotating based on input direction and camera angle. Works better for 3D, 2D uses sprite billboard instead
-            //transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            characterController.Move(moveDirection.normalized * Time.deltaTime * speed);
+            velocity = -1.0f;
         }
-        
-        
-        
-
-
+        else 
+        {
+            velocity += gravity * gravityMultiplier * Time.deltaTime;
+        }
+        moveDirection.y = velocity;
     }
+
+    private void ApplyRotation()
+    {
+        if (_input.sqrMagnitude == 0) return;
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+    }
+
+    private void ApplyMovement() 
+    {
+        _characterController.Move(moveDirection * speed * Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+        Debug.Log(_input);
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started) return;
+        if (!IsGrounded()) return;
+
+        velocity += jumpPower;
+    }
+    private bool IsGrounded() => _characterController.isGrounded;
 }
